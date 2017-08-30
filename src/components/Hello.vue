5f0b033fb3f1a78c1c67e69d3613712cf6573a6a
Shifeng Huang <shifeng.huang@hpe.com>
2017-08-30 21:12:15 +0800

    login

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,41 +1,128 @@
 <template>
 
+<v-layout row wrap>
+  <v-flex xs12 sm6 offset-sm3>
+    <v-card  :class="{gone: logined}">
+      <v-card-title>
+        <h5>Login</h5>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-layout row wrap>
+            <v-flex sm6>
+              <v-text-field
+                    name="username"
+                    label="username"
+                    v-model="username"
+                    light
+                  ></v-text-field>
+            </v-flex>
+            <v-flex sm6>
+              <v-text-field
+                    name="input-1"
+                    label="password"
+                    type="password"
+                    v-model="password"
+                    light
+                  ></v-text-field>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card-text>
+      <v-card-actions class="login-action">
+        <v-btn primary right @click="submit">Login</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-flex>
+  <v-flex xs12 sm6 offset-sm3>
+    <v-card  :class="{gone: !logined}">
+      <v-card-title>
+        <h5>Select</h5>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-layout row wrap>
+            <v-flex sm6>
+              <v-text-field
+                    name="build"
+                    label="build"
+                    v-model="build"
+                    light
+                  ></v-text-field>
+            </v-flex>
+            <v-flex sm6>
+              <v-text-field
+                    name="input-1"
+                    label="password"
+                    type="password"
+                    v-model="password"
+                    light
+                  ></v-text-field>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card-text>
+      <v-card-actions class="login-action">
+        <v-btn primary right @click="submit">Login</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-flex>
+</v-layout>
 
 
 </template>
 
 <script>
-// import axios from 'axios'
+import axios from 'axios'
+
+var instance = axios.create({
+  baseURL: 'https://rally1.rallydev.com/slm/webservice/v2.0/'
+})
+
 export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      username: 'shifeng.huang@hpe.com',
+      password: '!QAZ@WSX3edc',
+      build: '',
+      testSet: '',
+      logined: false
     }
   },
 
-  created () {
+  methods: {
+    submit () {
+      instance.get('/security/authorize', {
+        headers: {'Authorization': 'Basic ' + btoa(this.username + ':' + this.password)}
+      })
+        .then(resp => {
+          console.log(resp)
+          this.logined = true
+
+          instance.defaults.headers.common['Authorization'] =
+            resp.data.OperationResult.SecurityToken
+        })
+        .then(() => {
+          // instance.get()
+        })
+        .catch(err => {
+          console.error(err)
+          this.logined = false
+        })
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.login-action {
+  justify-content: flex-end;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.gone {
+  display: none;
 }
 </style>
